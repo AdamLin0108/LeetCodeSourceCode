718ed5597612f4400e5df6c2adc5863e7e561173
Adam Lin <adam.lin@mpi.com.tw>
2024-10-31 10:25:13 +0800

    fix runtime error

--- 2095_deleteMiddle.cpp
+++ 2095_deleteMiddle.cpp
@@ -73,104 +73,18 @@ int main(){
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        // If the list is empty, return nullptr
-        if (head == nullptr) return nullptr;
-
-        // If the list has only one node, delete it and return nullptr
-        if (head->next == nullptr) {
-            delete head;
-            return nullptr;
-        }
-
-        // Create a dummy node pointing to the head
+        if(head == nullptr) return nullptr;
         ListNode* prev = new ListNode(0);
+
         prev->next = head;
+        ListNode* slow = prev;
+        ListNode* fast = head;
 
-        // Initialize two pointers: slow and fast
-        ListNode* slow = prev; // Will end up at the node before the middle
-        ListNode* fast = head; // Moves twice as fast as slow
-
-        /* Initial Diagram:
-        
-        prev -> [0] -> [1] -> [2] -> [3] -> [4] -> [5] -> nullptr
-                   |
-                  slow
-                   |
-                  head
-                   |
-                  fast
-
-        */
-
-        // Traverse the list
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;           // Move slow by one node
-            fast = fast->next->next;     // Move fast by two nodes
-
-            /* Diagram after each iteration:
-
-            Iteration 1:
-            slow moves to head (node 1)
-            fast moves to node 3
-
-            prev -> [0] -> [1] -> [2] -> [3] -> [4] -> [5] -> nullptr
-                          |
-                         slow
-                                    |
-                                   fast
-
-            Iteration 2:
-            slow moves to node 2
-            fast moves to node 5
-
-            prev -> [0] -> [1] -> [2] -> [3] -> [4] -> [5] -> nullptr
-                                 |
-                                slow
-                                               |
-                                              fast
-
-            Iteration 3:
-            slow moves to node 3
-            fast moves to nullptr (end of list)
-
-            prev -> [0] -> [1] -> [2] -> [3] -> [4] -> [5] -> nullptr
-                                          |
-                                         slow
-                                                       |
-                                                      fast (nullptr)
-
-            */
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
         }
-
-        // Now, slow->next is the middle node
-        // Delete the middle node
-        ListNode* temp = slow->next;      // Store the middle node
-        slow->next = slow->next->next;    // Bypass the middle node
-
-        /* Diagram after deletion:
-
-        Middle node (node 3) is deleted
-
-        prev -> [0] -> [1] -> [2] -------> [4] -> [5] -> nullptr
-                                          /
-                                  [3] (deleted)
-        
-        Updated positions:
-
-        slow remains at node 3 (which is now connected to node 4)
-
-        */
-
-        delete temp;                      // Free memory of the middle node
-
-        // Get the new head of the list
-        ListNode* newHead = prev->next;
-
-        // Delete the dummy node
-        delete prev;
-
-        // Return the modified list
-        return newHead;
+        slow->next = slow->next->next;
+        return prev->next;
     }
 };
-
