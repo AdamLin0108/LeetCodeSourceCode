2a34f4565e974997e06ccab3d46cb802b364bb82
Adam Lin <adam.lin@mpi.com.tw>
2024-11-01 21:09:19 +0800

    INT_MAX

---
+++ 334_increasingTriplet.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n1 = INT_MAX, n2 = INT_MAX;
+
+        for(int i = 0; i < nums.size(); ++i){
+            if(nums[i] <= n1){
+                n1 = nums[i];
+            } else if(nums[i] <= n2){
+                n2 = nums[i];
+            } else if(nums[i] >= n2){
+                return true;
+            }
+        }
+        return false;
+    }
+};
